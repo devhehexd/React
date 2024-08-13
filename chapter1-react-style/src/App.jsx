9ba349b4ddbb4ff2fb536cb8a7c6c42d0beba0c0
cruz <rlqja72201@gmail.com>
2024-08-13 17:27:54 +0900

    8월 13일 React 수업

---
+++ chapter1-react-style/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <h1>Let&apos;s go React</h1>
+      <p>Hello, World</p>
+      <img src="images/sadcatgg.jpg" />
+    </div>
+  )
+}
+
+export default App
---
+++ chapter1-react-style/src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//기본 설정은 여기에
+createRoot(document.getElementById('root')).render(
+  <App />
+)
