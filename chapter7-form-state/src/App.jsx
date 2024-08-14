36225528c224773879da80a367a4f9af1da75d14
cruz <rlqja72201@gmail.com>
2024-08-14 17:34:08 +0900

    8월 14일 React 수업

---
+++ chapter7-form-state/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Register from './components/Register'
+//import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <Register></Register>
+    </>
+  )
+}
+
+export default App
