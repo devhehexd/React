0a6043a37d6a965a30672070a2708fac373a8373
cruz <rlqja72201@gmail.com>
2024-09-13 17:40:12 +0900

    9월 13일 React 수업

---
+++ react-project/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Timer from './components/useEffect/Timer'
+import CounterWithoutRef from './components/useRef/CounterWithoutRef'
+import CounterWithRef from './components/useRef/CounterWithRef'
+import InputWitoutRef from './components/useRef/InputWithoutRef'
+import InputWithRef from './components/useRef/InputWithRef'
+import Counter from './components/useEffect/Counter'
+import TodoRoot from './components/contextapi/TodoRoot'
+
+
+function App() {
+  return (
+    <>
+      <TodoRoot></TodoRoot>
+    </>
+  )
+}
+
+export default App
